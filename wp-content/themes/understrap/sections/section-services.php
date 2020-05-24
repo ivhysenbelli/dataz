9d4a80848dd518cf2b3c6255390a85a3b28c89ad
Ivi Hysenbelli <iv.hysenbelli@gmail.com>
2020-05-24 15:13:46 +0200

    Services

---
+++ wp-content/themes/understrap/sections/section-services.php
@@ -0,0 +1,35 @@
+<section class="section section-services">
+	<div class="container">
+		<div class="row">
+			<?php if(have_rows('services')): ?>
+				<?php while(have_rows('services')) : the_row(); ?>
+					<div class="row single-service">
+						<div class="col-lg-6">
+							<div class="data">
+								<h2 class="title"><?php the_sub_field('section_title'); ?></h2>
+								<p class="description"><?php the_sub_field('section_description'); ?></p>
+							</div>
+							<div class="buttons-row">
+								<a href="<?php the_sub_field('button_url') ?>" class="button"><?php the_sub_field('button_link') ?></a>
+							</div>
+							<?php $extraInfo = get_sub_field_object('show_extra_info'); ?>
+							<?php if($extraInfo['value'] == true): ?>
+								<div class="extra-info">
+									<ul class="clients-list">
+									<?php while(have_rows('extra_info')): the_row(); ?>
+										<li><?php the_sub_field('clients') ?></li>
+									<?php endwhile; ?>
+									</ul>
+								</div>
+							<?php endif; ?>
+						</div>
+						<div class="col-lg-6 img-col">
+							<?php $image = get_sub_field('image'); ?>
+							<img src="<?php echo $image['url']; ?>" alt="">
+						</div>
+					</div>
+				<?php endwhile; ?>
+			<?php endif; ?>
+		</div>
+	</div>
+</section>
